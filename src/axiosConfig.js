acaddff4ffccd9d6a6ed42d6200d59c0309bff94
TiLux257 <work.lethanhtien@gmail.com>
2023-02-22 11:08:51 +0700

    change baseURL

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://demo-fido-app.herokuapp.com/',
+    baseURL: 'https://fido-app-api.onrender.com/',
 })
